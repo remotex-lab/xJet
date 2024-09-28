774bd07e20acce8c10f64473b7d4a378f50e697b
Garefild <Garefild2@gmail.com>
2024-09-28 04:04:08 +0300

    Init build system

---
+++ jest.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.tsx?$': [ '@swc/jest' ],
+    },
+    collectCoverageFrom: [
+        'src/**/*.{ts,tsx,js,jsx}',
+        '!**/*.d.ts',
+    ],
+    testPathIgnorePatterns: [ '/lib/', '/node_modules/', '/dist/' ],
+    moduleNameMapper: {
+        '^@providers/(.*)$': '<rootDir>/src/providers/$1',
+    },
+};
