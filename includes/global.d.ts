89501a279cba86bef954e5d329c6b2f4d1846d70
Garefild <Garefild2@gmail.com>
2025-03-16 22:35:17 +0200

    global.d: Add global types for runtime config and dispatch function

--- includes/global.d.ts
+++ includes/global.d.ts
@@ -1,4 +1,10 @@
 /**
+ * Import will remove at compile time
+ */
+
+import type { RuntimeConfigInterface } from '@adapters/interfaces/base-adapter.interface';
+
+/**
  * Globals
  */
 
@@ -8,6 +14,18 @@ declare global {
      */
 
     const __VERSION: string;
+
+    /**
+     * xJet runtime config
+     */
+
+    const __XJET__: RuntimeConfigInterface;
+
+    /**
+     * Dispatch message result from test runner
+     */
+
+    function dispatch(data: Buffer): void;
 }
 
 export {};
